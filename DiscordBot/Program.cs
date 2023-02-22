3267b74ff21f3ab2a48879029b0e07d7b32ee7d5
Cory Fabian <Coryf65@users.noreply.github.com>
2023-02-22 12:36:28 -0600

    setting up discord bot and config file

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -1,8 +1,23 @@
 using DiscordBot;
+using DSharpPlus;
 
 IHost host = Host.CreateDefaultBuilder(args)
-	.ConfigureServices(services =>
+	.ConfigureServices((builder, services) =>
 	{
+		// Facotry method, initializing client
+		services.AddSingleton<DiscordClient>((serviceProvider) =>
+		{
+			// getting config files
+			var configuration = builder.Configuration;
+
+			var discordClient = new DiscordClient(new DiscordConfiguration
+			{
+				Token = configuration["DiscordBot:Token"],
+				TokenType = TokenType.Bot,
+				Intents = DiscordIntents.AllUnprivileged
+			});
+			return discordClient;
+		});
 		services.AddHostedService<Worker>();
 	})
 	.Build();
