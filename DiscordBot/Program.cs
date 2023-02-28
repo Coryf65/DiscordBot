980fbd3653b588d0b3f0aee01c1397c9aad6bc59
Cory Fabian <Coryf65@users.noreply.github.com>
2023-02-27 23:21:31 -0600

    added Serilog added logging

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -1,11 +1,35 @@
 using DiscordBot;
+using DiscordBot.Admin;
 using DiscordBot.BotInfo;
+using DiscordBot.Testing;
 using DSharpPlus;
+using DSharpPlus.Exceptions;
 using DSharpPlus.SlashCommands;
+using DSharpPlus.SlashCommands.EventArgs;
+using Serilog;
+using Serilog.Formatting.Json;
 
 const ulong GUILD_ID = 253627388754264065;
 
-IHost host = Host.CreateDefaultBuilder(args)
+// creating the Serilog ILogger
+Log.Logger = new LoggerConfiguration()
+	.WriteTo.Console()
+	.WriteTo.File(path: "logs/logs.txt",
+				restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information,
+				rollingInterval: RollingInterval.Day) // setting file log levels to the file
+	.WriteTo.File(new JsonFormatter(), path: "logs/jsonlogs.json",
+				restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information,
+				rollingInterval: RollingInterval.Day) // JSON formatter
+	.WriteTo.File(path: "logs/errors.txt",
+				restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Warning) // will only write Errors to this file
+	.CreateLogger();
+
+try
+{
+	Log.Information("Setting up Discord Bot");
+
+	IHost host = Host.CreateDefaultBuilder(args)
+	.UseSerilog()
 	.ConfigureServices((builder, services) =>
 	{
 		// Facotry method, initializing client
@@ -28,6 +52,8 @@ IHost host = Host.CreateDefaultBuilder(args)
 
 			slash.RegisterCommands<CoryTextCommands>(guildId: GUILD_ID);
 			slash.RegisterCommands<Info>(guildId: GUILD_ID);
+			slash.RegisterCommands<Chat>(guildId: GUILD_ID);
+			slash.SlashCommandErrored += Slash_SlashCommandErrored;
 
 			return discordClient;
 		});
@@ -35,4 +61,33 @@ IHost host = Host.CreateDefaultBuilder(args)
 	})
 	.Build();
 
-await host.RunAsync();
+	await host.RunAsync();
+
+}
+catch (Exception error)
+{
+	// log any errors while setting up the host
+	Log.Fatal(error, "Exception while setting up...");
+}
+finally
+{
+	// cleanup
+	Log.Information("Shutting down DiscordBot...");
+	Log.CloseAndFlush();
+}
+
+
+Task Slash_SlashCommandErrored(SlashCommandsExtension sender, SlashCommandErrorEventArgs e)
+{
+	Log.Error("Exception occured:\n" + e.Exception);
+	switch (e.Exception)
+	{
+		case BadRequestException ex:
+			Log.Error("Client BadRequestException:\n JSON Message: " + ex.JsonMessage);
+			break;
+		default:
+			Log.Error("Error: {error}", e.Exception);
+			break;
+	}
+	return Task.CompletedTask;
+}
