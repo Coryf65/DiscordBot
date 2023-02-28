93af401f69a137de4bd806aa625dbb6922ba628c
Cory Fabian <Coryf65@users.noreply.github.com>
2023-02-27 23:21:10 -0600

    new class for current games and servers info

---
+++ DiscordBot/Games/Servers.cs
@@ -0,0 +1,9 @@
+﻿using DSharpPlus.SlashCommands;
+
+namespace DiscordBot.Games;
+
+[SlashCommandGroup("servers", "Commands for info about the current Game Servers running or how to connect")]
+internal class Servers
+{
+
+}
